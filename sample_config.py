3af23fa36dcd4da5cc2ea1d0250fee692797f4e7
RaphaelKhaykin <raphaelkhaykin@gmail.com>
2021-09-19 01:38:41 -0400

    added sample config

---
+++ sample_config.py
@@ -0,0 +1,5 @@
+API_KEY = '' # Your app's Consumer Key 
+ACCOUNT_ID = '' # Your account number
+REDIRECT_URI = 'https://localhost'
+TOKEN_PATH = 'token.pickle'
+CHROMEDRIVER_PATH = 'chromedriver.exe' # path to chromedriver.exe
