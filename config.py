8e40745fb74e7cb03630ccd45a4a177e0ce0bc05
RaphaelKhaykin <raphaelkhaykin@gmail.com>
2021-09-19 01:35:02 -0400

    test gitignore

--- config.py
+++ config.py
@@ -3,3 +3,5 @@ ACCOUNT_ID = '' # Your account number
 REDIRECT_URI='https://localhost'
 TOKEN_PATH= 'token.pickle' 
 CHROMEDRIVER_PATH = 'chromedriver.exe' # path to chromedriver.exe
+
+#testing git
