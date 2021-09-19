3f8802aa3a26f2d90ffb66bd7418dfbdf35659b3
RaphaelKhaykin <raphaelkhaykin@gmail.com>
2021-09-19 01:34:07 -0400

    added gitignore

--- config.py
+++ config.py
@@ -1,7 +1,5 @@
 API_KEY = '' # Your app's Consumer Key 
 ACCOUNT_ID = '' # Your account number
 REDIRECT_URI='https://localhost'
-TOKEN_PATH= '' 
-CHROMEDRIVER_PATH = '' # path to chromedriver.exe
-
-#testing git
+TOKEN_PATH= 'token.pickle' 
+CHROMEDRIVER_PATH = 'chromedriver.exe' # path to chromedriver.exe
