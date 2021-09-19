d0778f62e7c9d6201f2bfebc1f276f8efc02cc09
RaphaelKhaykin <43918044+RaphaelKhaykin@users.noreply.github.com>
2021-09-19 01:35:47 -0400

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-API_KEY = '' # Your app's Consumer Key 
-ACCOUNT_ID = '' # Your account number
-REDIRECT_URI='https://localhost'
-TOKEN_PATH= 'token.pickle' 
-CHROMEDRIVER_PATH = 'chromedriver.exe' # path to chromedriver.exe
-
-#testing git
